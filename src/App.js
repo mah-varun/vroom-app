dd2f7134be7ea3b2ae5a8f04b73cd49986be81e0
v.mahajan <v.mahajan@salesforce.com>
2023-08-18 19:44:48 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Varun
+          Welcome Varun Mahajan
         </p>
         <a
           className="App-link"
